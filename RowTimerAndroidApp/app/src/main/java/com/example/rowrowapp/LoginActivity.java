356938f7235e3ff531d4794394351d7c5b5d2942
Bruno Freitas <freitascbruno@gmail.com>
2016-04-13 22:22:05 +0100

    Bruno's Branch Starting On LoginActivity added code to get the KeyHash that needs to be added for Facebook login

--- RowTimerAndroidApp/app/src/main/java/com/example/rowrowapp/LoginActivity.java
+++ RowTimerAndroidApp/app/src/main/java/com/example/rowrowapp/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.example.rowrowapp;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,6 +24,26 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
         setTitle(R.string.title_activity_login);
+
+        /**
+         // O código abaixo gera e mostra no separador 'Android Monitor -> LOGCAT' o KeyHash para ser adicionado
+         // na página facebook de configuração da aplicação (https://developers.facebook.com/apps/1340808619269535/settings/)
+
+         try {
+         PackageInfo info = getPackageManager().getPackageInfo(
+         "com.example.rowrowapp",
+         PackageManager.GET_SIGNATURES);
+         for (Signature signature : info.signatures) {
+         MessageDigest md = MessageDigest.getInstance("SHA");
+         md.update(signature.toByteArray());
+         Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+         }
+         } catch (PackageManager.NameNotFoundException e) {
+
+         } catch (NoSuchAlgorithmException e) {
+
+         }
+         */
     }
 
     @Override
